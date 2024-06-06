35632b3eba45d2ecd4cc1a696ba1255ff759cca7
rikiTada <riki.tada777@gmail.com>
2024-06-06 15:15:20 +0900

    fix: ページ追加による、layout修正

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,10 +1,22 @@
-import { ModeToggle } from "@/components/mode-toggle";
+import { ModeToggle } from "@/components/layout/mode-toggle";
+import { Slot } from "@radix-ui/react-slot";
+import { LucideIcon } from "lucide-react";
 
-export default function Header({ title }: { title: string }) {
+export default function Header({
+  title,
+  icon,
+}: {
+  title: string;
+  icon: React.ReactNode;
+}) {
+  const IconSlot = Slot as LucideIcon;
+
   return (
     <header className="container border-b border-zinc-400/10 mx-auto flex h-16 items-center justify-between">
-      {/* <div className="h-10 w-10"></div> */}
-      <h1 className="text-3xl font-bold">{title}</h1>
+      <div className="flex items-center gap-2.5">
+        <IconSlot size={40}>{icon}</IconSlot>
+        <h1 className="text-3xl font-bold">{title}</h1>
+      </div>
       <ModeToggle />
     </header>
   );
