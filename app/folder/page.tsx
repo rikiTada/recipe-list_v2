b93fb9295d0c885903c89dfe84b3caca06c1b7b4
rikiTada <riki.tada777@gmail.com>
2024-06-09 18:39:41 +0900

    feat: create data-table.

--- app/folder/page.tsx
+++ app/folder/page.tsx
@@ -1,6 +1,15 @@
+import DataTable from "@/components/data-table";
 import Header from "@/components/layout/header";
 import { Folder } from "lucide-react";
 
 export default function Page() {
-  return <Header title="レシピ一覧" icon={<Folder />} />;
+  return (
+    <>
+      <Header title="レシピ一覧" icon={<Folder />} />
+      <div className="container my-8">
+        <DataTable />
+        <div className="container h-16"></div>
+      </div>
+    </>
+  );
 }
---
+++ components/data-table.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  Table,
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+import { useRecipeData } from "@/hooks";
+import { useRecipeStore } from "@/store/recipeStore";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DataTable() {
+  const { recipeData } = useRecipeStore();
+  useRecipeData();
+
+  return (
+    <div>
+      <Table>
+        <TableCaption>- レシピ 一覧 -</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">登録日</TableHead>
+            <TableHead>タイトル</TableHead>
+            <TableHead className="min-w-[100px]">イメージ</TableHead>
+            <TableHead>タグ</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {recipeData.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.timeStamp}</TableCell>
+              <TableCell>
+                <Link href={`/folder/${item.id}`}>{item.title}</Link>
+              </TableCell>
+              <TableCell>
+                <Image
+                  unoptimized
+                  width={50}
+                  height={50}
+                  alt="recipe image"
+                  src={item.imgPath}
+                />
+              </TableCell>
+              <TableCell className="">
+                {item.tag.map((tag) => (
+                  <div key={tag}>{tag}</div>
+                ))}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
