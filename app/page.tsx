ebeea80576dde16574faf05c409a8b2a56ed189d
rikiTada <riki.tada777@gmail.com>
2024-06-09 18:25:32 +0900

    feat: create type & fix page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Header from "@/components/layout/header";
-import { refName, getSaveItems } from "@/lib";
 import { HomeIcon } from "lucide-react";
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <Header title="今日の晩ごはん" icon={<HomeIcon />} />
---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type RecipeList = {
+  id: string;
+  title: string;
+  timeStamp: string;
+  tag: string[];
+  url: string;
+  imgPath: string;
+  snippet: string;
+  mark: boolean;
+};
