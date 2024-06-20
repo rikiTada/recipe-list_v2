9268b47aad4a8f993f0fe28696effe60e0395f8a
rikiTada <riki.tada777@gmail.com>
2024-06-21 07:48:29 +0900

    feat: add storybook.

---
+++ stories/Header.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import Header from "@/components/layout/header";
+import { HomeIcon } from "lucide-react";
+
+const meta = {
+  title: "Header",
+  component: Header,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+  args: {
+    title: "Recipe List",
+    icon: <HomeIcon />,
+  },
+  argTypes: {
+    title: { control: "text" },
+    icon: { control: "object" },
+  },
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => <Header title="Recipe List" icon={<HomeIcon />} />,
+};
