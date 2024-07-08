ab969e66d3fa229295f0da65592fda0db00f636b
rikiTada <riki.tada777@gmail.com>
2024-07-09 08:01:01 +0900

    WIP: PrismaD1接続

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,19 @@
 import { handle } from "hono/vercel";
-import { app } from "@/app/api"
-
+import { app } from "@/app/api";
+import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
+import { D1Database } from "@cloudflare/workers-types";
 
 app.get("/", async (c) => {
-    return c.json({ message: "Hello, Hono🔥" });
-});
+  const adapter = new PrismaD1((c.env as { DB: D1Database }).DB);
+  const prisma = new PrismaClient({ adapter });
 
+  const users = await prisma.user.findMany();
+  return c.json(users);
+});
 
 app.get("/user", async (c) => {
   return c.json({ name: "John Doe" });
 });
-
 
 export const GET = handle(app);
--- app/api/index.ts
+++ app/api/index.ts
@@ -1,4 +1,9 @@
 import { Hono } from "hono";
+import { D1Database } from "@cloudflare/workers-types";
+
+export type Bindings = {
+  DB: D1Database;
+};
 
 export const runtime = "edge";
 export const app = new Hono().basePath("/api");
