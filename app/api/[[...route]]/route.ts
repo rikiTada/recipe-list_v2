81b034c10c6e4cbba211ea102d52402ee20e08c6
tada rikimaru <122006815+rikiTada@users.noreply.github.com>
2024-07-08 09:03:06 +0000

    routeの傍受実装

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,11 @@ import { handle } from "hono/vercel";
 import { app } from "@/app/api"
 
 
+app.get("/", async (c) => {
+    return c.json({ message: "Hello, Hono🔥" });
+});
+
+
 app.get("/user", async (c) => {
   return c.json({ name: "John Doe" });
 });
