867e3c6f61d1b8c057da01623cb4e1918a7e2e8e
rikiTada <riki.tada777@gmail.com>
2024-07-09 22:55:49 +0900

    wip: drizzle migrate

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,16 +1,5 @@
 import { handle } from "hono/vercel";
-import { PrismaClient } from "@prisma/client";
-import { PrismaD1 } from "@prisma/adapter-d1";
 import { D1Database } from "@cloudflare/workers-types";
-import { Hono } from "hono";
-
-export type Bindings = {
-  DB: D1Database;
-};
-
-export const runtime = "edge";
-const app = new Hono<{ Bindings: Bindings}>().basePath('/api')
-
 import { Hono } from "hono";
 
 export type Bindings = {
@@ -21,9 +10,11 @@ export const runtime = "edge";
 const app = new Hono().basePath("/api");
 
 app.get("/", async (c) => {
-  const { results } = await c.env.DB.prepare("SELECT * FROM users").all()
-  // const { results } = await process.env.DB.prepare("SELECT * FROM customers").all()
-  return c.json(results)
+  const { results } = await c.env.DB.prepare("SELECT * FROM users").all();
+  // const { results } = await process.env.DB.prepare(
+  //   "SELECT * FROM customers"
+  // ).all();
+  return c.json(results);
 });
 
 app.get("/user", async (c) => {
