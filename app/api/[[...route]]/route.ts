f10544d56c317e50e619b183276bc7fdcb6bf8f3
rikiTada <riki.tada777@gmail.com>
2024-07-09 22:13:04 +0900

    chore: add drizzle-kit drizzle-orm -D

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,8 +1,16 @@
 import { handle } from "hono/vercel";
-import { app } from "@/app/api";
 import { PrismaClient } from "@prisma/client";
 import { PrismaD1 } from "@prisma/adapter-d1";
 import { D1Database } from "@cloudflare/workers-types";
+
+import { Hono } from "hono";
+
+export type Bindings = {
+  DB: D1Database;
+};
+
+export const runtime = "edge";
+const app = new Hono().basePath("/api");
 
 app.get("/", async (c) => {
   const adapter = new PrismaD1((c.env as { DB: D1Database }).DB);
