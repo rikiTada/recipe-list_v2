04ae3ace5e3a07b0ac72ec4c1729c2621403ce82
tada rikimaru <122006815+rikiTada@users.noreply.github.com>
2024-07-17 03:45:05 +0000

    WIP: drrizzle ⇒ prisma

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,36 +3,51 @@ import { D1Database } from "@cloudflare/workers-types";
 import { Hono } from "hono";
 import { drizzle } from "drizzle-orm/d1";
 import { users } from "@/schema";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 
-export type Bindings = {
-  DB: D1Database;
-};
+// export type Bindings = {
+//   DB: D1Database;
+// };
 
-export const runtime = "edge";
-const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
+// export const runtime = "edge";
+// const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
 
-// app.get("/", async (c) => {
+// app.get("/users", async (c) => {
 //   try {
-//     let { results } = await c.env.DB.prepare("SELECT * FROM users").all();
-//     return c.json(results);
+
+//     const { DB } = getRequestContext().env;
+//     // const res = await DB.prepare;
+//     const db = drizzle(DB);
+//     // const db = drizzle(process.env.DB);
+//     const result = await db.select().from(users).all();
+//     return c.json(result);
 //   } catch (error) {
 //     return c.json({ error: (error as Error).message });
 //   }
 // });
 
-app.get("/users", async (c) => {
-  try {
-    const db = drizzle(c.env.DB);
-    // const db = drizzle(process.env.DB);
-    const result = await db.select().from(users).all();
-    return c.json(result);
-  } catch (error) {
-    return c.json({ error: (error as Error).message });
-  }
-});
-
 // app.get("/users", async (c) => {
 //   return c.json({ name: "John Doe" });
 // });
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
+import { Bindings } from "@/app/api";
+
+
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
+
+app.get("/", async (c) => {
+  const adapter = new PrismaD1(c.env.DB);
+  const prisma = new PrismaClient({ adapter });
+
+  const users = await prisma.user.findMany();
+
+  console.log(users);
+  return c.json(users);
+});
+
+export default app;
+
 
 export const GET = handle(app);
