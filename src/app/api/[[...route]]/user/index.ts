311ef00bde68c29aba31e02d71f9d2628eee2bba
rikiTada <riki.tada777@gmail.com>
2024-07-26 01:21:09 +0900

    UserAPI:GET/POST作成

--- src/app/api/[[...route]]/user/index.ts
+++ src/app/api/[[...route]]/user/index.ts
@@ -1,12 +1,31 @@
-import { createApp, prisma } from "@/lib/hono";
+import { createApp } from "@/lib/hono";
+import { db } from "@/app/api/[[...route]]/_service";
+import { Prisma } from "@prisma/client";
 
-const app = createApp().get("/", async (c) => {
+const app = createApp();
+
+app.get("/", async (c) => {
   try {
-    const users = await prisma.user.findMany();
-    return c.json({ users }, 200);
+    const data = await db.user.getAllUser();
+    return c.json({ data }, 200);
+  } catch (e) {
+    return c.json({ message: "system error", error: e }, 500);
+  }
+});
+
+app.post("/", async (c) => {
+  try {
+    const { username, email } = await c.req.json<Prisma.UserCreateInput>();
+
+    if (!username || !email) {
+      throw new Error("username and email are required");
+    }
+
+    const data = await db.user.createUser({ username, email });
+    return c.json({ data }, 200);
   } catch (e) {
     console.error(e);
-    return c.json({ message: "system error" }, 500);
+    return c.json({ message: "system error", error: e }, 500);
   }
 });
 
