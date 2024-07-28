20f931176c675ad26507f984803d32504b4df033
rikiTada <riki.tada777@gmail.com>
2024-07-28 23:20:39 +0900

    WIP: リストコンポーネント

--- src/components/page/shopping-list/listItem.tsx
+++ src/components/page/shopping-list/listItem.tsx
@@ -1,23 +1,34 @@
 import { Checkbox } from "@/components/ui/checkbox";
+import React from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 import { ShoppingList } from "@prisma/client";
 import { useState } from "react";
 
 export default function ListItem(item: ShoppingList) {
+  const { width, height } = useWindowSize();
   const [isChecked, setIsChecked] = useState(item.isPurchased);
 
   return (
     <div
       key={item.id}
-      className="flex items-center justify-between p-4 border-b border-gray-200"
+      className="flex items-center p-2.5 gap-2.5 border-b border-gray-200"
     >
-      <div className="flex gap-4 items-center">
-        <Checkbox
-          checked={isChecked}
-          onClick={() => setIsChecked(!isChecked)}
-        />
-        <p className="text-lg">{item.title}</p>
-      </div>
+      {isChecked && <Confetti width={width} height={height} recycle={false} />}
+
+      <Checkbox
+        id={item.id}
+        className="data-[state=checked]:bg-orange-500 border-gray-400"
+        checked={isChecked}
+        onClick={() => setIsChecked(!isChecked)}
+      />
+      <label
+        htmlFor={item.id}
+        className={`text-sm ${!isChecked ? "" : "text-zinc-400 line-through"}`}
+      >
+        {item.title}
+      </label>
     </div>
   );
 }
--- src/components/page/shopping-list/shoppingListPage.tsx
+++ src/components/page/shopping-list/shoppingListPage.tsx
@@ -7,30 +7,57 @@ import {
 import { Input } from "@/components/ui/input";
 import ListItem from "./listItem";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { Plus } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function ShoppingListPage() {
   const { data, error, isLoading, mutate } = useGetAllShoppingList();
-  const [InputValue, setInputValue] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <>
-      {data?.data.map((item) => (
-        <div key={item.id}>
-          <ListItem {...item} />
+    <div className="container">
+      <div className="my-4 flex gap-4">
+        <Input
+          placeholder="リストを追加"
+          type="text"
+          ref={inputRef}
+          className="h-8 bg-stone-100 border-0 focus-visible:bg-background placeholder:text-stone-300"
+        />
+        <div>
+          <Button
+            onClick={() => {
+              if (!inputRef.current?.value) return; //TODO: toastでエラー表示
+              createShoppingList({ title: inputRef.current?.value });
+              mutate();
+            }}
+            variant="outline"
+            size="icon"
+            className="size-8 bg-orange-400 "
+          >
+            <Plus size={24} className="text-primary-foreground" />
+          </Button>
         </div>
-      ))}
-      <div className="container my-4 flex gap-4">
-        <Input type="text" onChange={(e) => setInputValue(e.target.value)} />
-        <Button
-          onClick={() => {
-            createShoppingList({ title: InputValue });
-            mutate();
-          }}
-        >
-          追加
-        </Button>
       </div>
-    </>
+      {["スーパー", "日用品", "100均", "その他"].map((name) => (
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>{name}</AccordionTrigger>
+            <AccordionContent className="h-40 overflow-auto">
+              {data?.data.map((item) => (
+                <div key={item.id}>
+                  <ListItem {...item} />
+                </div>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      ))}
+    </div>
   );
 }
